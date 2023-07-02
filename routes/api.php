0bd3899501f4fb07d2db7d04b322ea933a47f316
ACH RIZAL <achrizal15@gmail.com>
2023-07-02 15:56:28 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Api\CommentController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
-|
+| 
 */
 
 Route::get('/comment',[CommentController::class,"index"]);
