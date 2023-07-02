4286c06b702ca103d8db1cbf57131e9e63de9b80
ACH RIZAL <achrizal15@gmail.com>
2023-07-02 16:00:16 +0700

    Merge branch 'rizal-dev'

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        "uuid",
+        "detail",
+        "name",
+        "presence",
+        "parent_id"
+    ];
+    public function comments ()
+    {
+        return $this->hasMany(Comment::class, "parent_id", "uuid");
+    }
+    public function parent ()
+    {
+        return $this->belongsTo(Comment::class, "parent_id", "uuid");
+    }
+}
